4a0b90d63b9589527c1c1b4cf2fe3c6362d9a652
lunsovathana <sovathanalun@gmail.com>
2016-06-21 15:52:43 +0700

    add welcome controller

--- src/main/java/com/spring/config/ServletConfig.java
+++ src/main/java/com/spring/config/ServletConfig.java
@@ -1,17 +1,28 @@
 package com.spring.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
 @Configuration
 @EnableWebMvc
 @ComponentScan(basePackages="com.spring.controller")
-public class ServletConfig {
+public class ServletConfig extends WebMvcConfigurerAdapter{
 	
 	/*@Bean
 	public WelcomeController welcome(){
 		return new WelcomeController();
 	}*/
+	
+	@Bean
+	public InternalResourceViewResolver resolver(){
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setPrefix("/WEB-INF/jsp/");
+		resolver.setSuffix(".jsp");
+		return resolver;
+	}
 	
 }
